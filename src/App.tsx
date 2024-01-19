bfde145746b67c11d3edaaedb0ddf6e7e30c6970
K. Arrayyan <khanza_arrayyan@yahoo.co.id>
2024-01-19 20:51:59 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => {
+  return (
+    <h1 className="font-space-mono text-3xl font-bold underline hover:cursor-pointer">
+      Hello, World!
+    </h1>
+  );
+};
+
+export default App;
